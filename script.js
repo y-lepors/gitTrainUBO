e6fe8325231dfb7efdf2ef2b32c1e9847e578dfa
yanis <yanis.lepors@gmail.com>
2023-01-11 10:30:56 +0100

    new object

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ let b = 45;
 
 b += a ;
 console.log("resultat de a + b = " + b);
+
+let aPerson = { 
+	name: 'Jean',
+	age: 35,
+	gender: 'masculin',
+	interest: ['musique', 'Jeux videos'],
+};
+
+aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
+
+aPerson.sayHello();
